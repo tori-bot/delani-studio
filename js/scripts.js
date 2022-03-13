fdbd8a9ef6c84cd868e271a6752db0327d82366f
victoria makena <makenavictoria1@gmail.com>
2022-03-14 00:09:17 +0300

    add hide and show methods to all elements in 'what we do' div

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 
 $(document).ready(function () {
-    $("#clickable").click(function () {
-        $("#clickable").hide();
-        $("#explainer").show();
+    $("#des-clickable").click(function () {
+        $("#des-clickable").hide();
+        $("#des-explainer").show();
     });
-    $("#explainer").click(function () {
-        $("#explainer").hide();
-        $("#clickable").show();
+    $("#des-explainer").click(function () {
+        $("#des-explainer").hide();
+        $("#des-clickable").show();
+    });
+});
+$(document).ready(function () {
+    $("#dev-clickable").click(function () {
+        $("#dev-clickable").hide();
+        $("#dev-explainer").show();
+    });
+    $("#dev-explainer").click(function () {
+        $("#dev-explainer").hide();
+        $("#dev-clickable").show();
+    });
+});
+$(document).ready(function () {
+    $("#pm-clickable").click(function () {
+        $("#pm-clickable").hide();
+        $("#pm-explainer").show();
+    });
+    $("#pm-explainer").click(function () {
+        $("#pm-explainer").hide();
+        $("#pm-clickable").show();
     });
 });
