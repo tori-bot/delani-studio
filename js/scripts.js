e4105ed8452c613837289e5615fe7a046acd74de
victoria makena <makenavictoria1@gmail.com>
2022-03-14 07:51:57 +0300

    add jquery arguements for hoverfunction

--- js/scripts.js
+++ js/scripts.js
@@ -30,13 +30,59 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $("#work4").mouseover(function () {
-        $("#reveal4").show();
+$(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $(".card-body4").show();
+    }).mouseout(function(){
+      $(".card-body4").hide();
     });
-    mouseleave(function () {
-        $("#reveal4").hide();
-    });
-    
-       
 });
+$(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $(".card-body3").show();
+    }).mouseout(function(){
+      $(".card-body3").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $(".card-body2").show();
+    }).mouseout(function(){
+      $(".card-body2").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $(".card-body1").show();
+    }).mouseout(function(){
+      $(".card-body1").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $(".card-body5").show();
+    }).mouseout(function(){
+      $(".card-body5").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work6").mouseover(function(){
+      $(".card-body6").show();
+    }).mouseout(function(){
+      $(".card-body6").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work7").mouseover(function(){
+      $(".card-body7").show();
+    }).mouseout(function(){
+      $(".card-body7").hide();
+    });
+});
+$(document).ready(function(){
+    $("#work8").mouseover(function(){
+      $(".card-body8").show();
+    }).mouseout(function(){
+      $(".card-body8").hide();
+    });
+  });
